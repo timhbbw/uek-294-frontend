bff57f9366cd0f2460804f76bcae34869a616d2f
Tim <tim.hug@digitecgalaxus.ch>
2026-08-31 11:34:01 +0200

    task 1.3 done ig?

--- block1/task1.3/exampleJs.js
+++ block1/task1.3/exampleJs.js
@@ -1,6 +1,9 @@
 function doSomethingGreat(name) {
     alert('Do something Great was called');
 }
+function customAlert(message) {
+    alert(message);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     let myButton = document.getElementById('MyButton');
@@ -10,11 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let myButton5 = document.getElementById('MyButton5');
     let myButton6 = document.getElementById('MyButton6');
     myButton.onclick = function () { alert('Hallo button'); }
-    myButton2.onclick = doSomethingGreat;
-    myButton3.onclick = customAlert("HUHHH")
-    myButton4.onclick = customAlert("Wowza")
+    myButton2.onclick = () => doSomethingGreat;
+    myButton3.onclick = () => customAlert("HUHHH");
+    myButton4.onclick = () => customAlert("Wowza");
+    myButton5.onclick = () => customAlert("this is insane");
+    myButton6.onclick = () => customAlert("The final one");
 })
-
-function customAlert(alert){
-    alert(alert)
-}
