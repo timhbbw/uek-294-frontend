1f1f63d80bb4fd47f5ccbde9f8ef31e504b0653f
Tim <tim.hug@digitecgalaxus.ch>
2026-09-01 10:46:02 +0200

    task 3.2.1/2 done

--- block1/task1.2/spoofify.js
+++ block1/task1.2/spoofify.js
@@ -1,2 +1,31 @@
-let addmore = document.getElementById('addmore')
-let removealbum = document.getElementById('rem')
+let addmore = document.getElementById('addmore');
+let removealbum = document.getElementById('remfirst');
+addmore.addEventListener('click', addmorealbums);
+removealbum.addEventListener('click', removefirstalbums);
+let releases = document.getElementsByClassName('releases-content')[0];
+
+function createalbum() {
+    const newalbum = document.createElement('div');
+    newalbum.className = 'releases-release';
+
+    const cover = document.createElement('img');
+    cover.src = 'https://loremflickr.com/480/480/cd,album,cover?random=';
+
+    newalbum.append(cover);
+    return newalbum;
+}
+
+function addmorealbums() {
+    releases.append(createalbum());
+    releases.append(createalbum());
+    releases.append(createalbum());
+}
+
+function removefirstalbums() {
+    let albums = document.getElementsByClassName('releases-release');
+    for (let i = 0; i < 3; i++) {
+        if (albums.length > 0) {
+            albums[0].remove();
+        }
+    }
+}
