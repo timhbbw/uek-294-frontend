c9260f1711fdffe95f8031a80cc6d8e2916436bb
Tim <tim.hug@digitecgalaxus.ch>
2026-09-01 13:37:12 +0200

    nocie

--- block3/task3.4/script.js
+++ block3/task3.4/script.js
@@ -1,4 +1,10 @@
 function selectRandomCountry() {
-    
-    Math.floor(Math.random() * countries.length);
+    const destinations = document.querySelectorAll("#destinations li");
+    const selected = Math.floor(Math.random() * destinations.length);
+    const country = destinations[selected];
+    destinations.forEach((destination) => destination.classList.remove("selected"));
+    country.classList.add("selected");
+    country.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
 }
+
+document.getElementById("choose").addEventListener("click", selectRandomCountry);  
