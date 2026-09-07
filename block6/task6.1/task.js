90f513533ed1f4c7139737bd83be125bcef640df
Tim <tim.hug@digitecgalaxus.ch>
2026-09-07 13:54:39 +0200

    fixed

--- block6/task6.1/task.js
+++ block6/task6.1/task.js
@@ -1,21 +1,25 @@
 
 const form = document.forms.signup;
 
-const prenameelement = form.elements.prename;
-const prename = prenameelement.value;
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-let datetime = new Date();
-const currenthour = datetime.getHours();
+    const prenameelement = form.elements.prename;
+    const prename = prenameelement.value;
 
-if (currenthour >= 5 && currenthour < 11) {
-    alert('Guten Morgen ' + prename);
-} else if (currenthour >= 11 && currenthour < 18) {
-    alert('Guten Tag ' + prename);
-} else if (currenthour >= 18 && currenthour < 22) {
-    alert('Guten Abend ' + prename);
-} else {
-    alert('Gute Nacht ' + prename);
-}
+    let datetime = new Date();
+    const currenthour = datetime.getHours();
 
-console.log(username);
-console.log(currenthour);
+    if (currenthour >= 5 && currenthour < 11) {
+        alert('Guten Morgen ' + prename);
+    } else if (currenthour >= 11 && currenthour < 18) {
+        alert('Guten Tag ' + prename);
+    } else if (currenthour >= 18 && currenthour < 22) {
+        alert('Guten Abend ' + prename);
+    } else {
+        alert('Gute Nacht ' + prename);
+    }
+
+    console.log(prename);
+    console.log(currenthour);
+});
