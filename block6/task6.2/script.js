52a49b57233838c0175509de459437e698aba1e1
Tim <tim.hug@digitecgalaxus.ch>
2026-09-07 15:22:00 +0200

    base

---
+++ block6/task6.2/script.js
@@ -0,0 +1,38 @@
+
+const form = document.forms.password;
+
+    const emailelement = form.elements.email;
+    
+    const passwordelement = form.elements.password;
+    
+    const confirmpasswordelement = form.elements['confirm-password'];
+    
+    const termselement = form.elements.terms;
+    
+form.addEventListener('submit', function (event) {
+
+    const email = emailelement.value;
+    const password = passwordelement.value;
+    const confirmpassword = confirmpasswordelement.value;
+    const terms = termselement.checked;
+
+    confirmpasswordelement.setCustomValidity('');
+   
+    if (!terms){
+        termselement.setCustomValidity('You must agree to the terms and conditions.');
+    }
+
+    if (password.length < 8){
+        passwordelement.setCustomValidity('Password must be at least 8 characters long.');
+    }
+
+    if (password !== confirmpassword){
+        confirmpasswordelement.setCustomValidity('Passwords do not match.');
+    }
+    
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        form.reportValidity();
+    }
+    
+});
