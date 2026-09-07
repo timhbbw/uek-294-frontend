f027f3577f63a4149037dc14adf345a54be1c030
Tim <tim.hug@digitecgalaxus.ch>
2026-09-07 16:05:29 +0200

    Musterlösung done

--- block6/task6.2/script.js
+++ block6/task6.2/script.js
@@ -1,34 +1,35 @@
 
 const form = document.forms.password;
-    
-    const passwordelement = form.elements.password;
-    
-    const confirmpasswordelement = form.elements['confirm-password'];
-    
-    const termselement = form.elements.terms;
-    
+
+const passwordelement = form.elements.password;
+
+const confirmpasswordelement = form.elements['confirm-password'];
+
+
+const termselement = form.elements.terms;
+
 form.addEventListener('submit', function (event) {
     const password = passwordelement.value;
     const confirmpassword = confirmpasswordelement.value;
     const terms = termselement.checked;
 
     confirmpasswordelement.setCustomValidity('');
-   
-    if (!terms){
+
+    if (!terms) {
         termselement.setCustomValidity('You must agree to the terms and conditions.');
     }
 
-    if (password.length < 8){
+    if (password.length < 8) {
         passwordelement.setCustomValidity('Password must be at least 8 characters long.');
     }
 
-    if (password !== confirmpassword){
+    if (password !== confirmpassword) {
         confirmpasswordelement.setCustomValidity('Passwords do not match.');
     }
-    
+
     if (!form.checkValidity()) {
         event.preventDefault();
         form.reportValidity();
     }
-    
+
 });
